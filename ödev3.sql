52bfae1679274123ed1b8fa3378eaf8c810096bc
Muhammed KILIÇ <105243448+mhmmdklc@users.noreply.github.com>
2022-06-27 20:40:03 +0300

    Update ödev3.sql

--- ödev3.sql
+++ ödev3.sql
@@ -8,6 +8,6 @@ SELECT * FROM country WHERE country ILIKE 'A%a';
 
 SELECT Country FROM Customers WHERE Country LIKE '____%n';
 
-SELECT title FROM film WHERE title ILIKE 'tttt'
+SELECT title FROM film WHERE title ILIKE '%t%t%t%t%'
 
 SELECT * FROM film WHERE title LIKE 'C%' AND length > 90 AND rental_rate = 2.99;
