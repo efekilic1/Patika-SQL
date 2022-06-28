a9ea85b16bbb1d0d67530a202e4399c457a176ec
Muhammed KILIÇ <105243448+mhmmdklc@users.noreply.github.com>
2022-06-28 19:27:34 +0300

    Update ödev4.sql

--- ödev4.sql
+++ ödev4.sql
@@ -4,6 +4,7 @@
 --3.film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 --4.country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 --5.city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
+-- www.patika.dev
 
 
 
